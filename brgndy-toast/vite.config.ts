ef35583d776af5434394c68cc8e6ae0ba59a864e
brgndy <brgndy96@gmail.com>
2024-06-02 22:56:31 +0900

    chore: 테스트 환경 구축

--- brgndy-toast/vite.config.ts
+++ brgndy-toast/vite.config.ts
@@ -1,10 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts(), libInjectCss()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
